175098003952bf6a21bf31abc386781ec82cdbf4
Nitestack <code@npham.de>
2025-10-30 20:47:54 +0100

    fix(hypridle): use `dms` instead of native loginctl to fix not unlocking bug

--- modules/home/hyprland/plugins/hypridle.nix
+++ modules/home/hyprland/plugins/hypridle.nix
@@ -1,19 +1,26 @@
 # ╭──────────────────────────────────────────────────────────╮
 # │ Hypridle                                                 │
 # ╰──────────────────────────────────────────────────────────╯
-{ pkgs, osConfig, ... }:
+{
+  pkgs,
+  osConfig,
+  flake,
+  ...
+}:
 let
+  inherit (flake) inputs;
+
   brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  dms = "${inputs.dms-cli.packages.${pkgs.system}.default}/bin/dms";
   hyprctl = "${osConfig.programs.hyprland.package}/bin/hyprctl";
-  loginctl = "${pkgs.systemd}/bin/loginctl";
 in
 {
   services.hypridle = {
     enable = true;
     settings = {
       general = {
-        lock_cmd = "${loginctl} lock-session";
-        before_sleep_cmd = "${loginctl} lock-session"; # lock before suspend
+        lock_cmd = "";
+        before_sleep_cmd = "${dms} ipc call lock lock"; # lock before suspend
         after_sleep_cmd = "${hyprctl} dispatch dpms on"; # to avoid having to press a key twice to turn on the display
       };
 
@@ -35,7 +42,7 @@ in
         }
         {
           timeout = 600; # 10min
-          on-timeout = "${loginctl} lock-session"; # lock screen when timeout has passed
+          on-timeout = "${dms} ipc call lock lock"; # lock screen when timeout has passed
         }
       ];
     };
