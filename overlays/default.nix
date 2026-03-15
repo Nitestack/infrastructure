45b6adc7929107cecb8a31a4c2cc8a7fc5bbe59a
Nitestack <code@npham.de>
2026-03-15 15:36:13 +0100

    refactor(nix): remove unused lib

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,5 @@
 # ╭──────────────────────────────────────────────────────────╮
 # │ Overlays                                                 │
 # ╰──────────────────────────────────────────────────────────╯
-_: final: prev: {
-  lib = prev.lib // {
-    scss = import ../lib/scss.nix {
-      inherit (prev) lib;
-      pkgs = final;
-    };
-  };
-}
+_: final: prev:
+{ }
