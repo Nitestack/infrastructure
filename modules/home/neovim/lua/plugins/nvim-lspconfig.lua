8955c1a57586e6c7e0b09b895a54b61ed9c67c95
Nitestack <code@npham.de>
2026-04-07 23:31:59 +0200

    feat(nvim): remove workspace diagnostics

--- modules/home/neovim/lua/plugins/nvim-lspconfig.lua
+++ modules/home/neovim/lua/plugins/nvim-lspconfig.lua
@@ -1,23 +1,11 @@
 ---@type LazyPluginSpec
 return {
   "neovim/nvim-lspconfig",
-  dependencies = {
-    {
-      "artemave/workspace-diagnostics.nvim",
-      opts = {},
-    },
-  },
   ---@param opts PluginLspOpts
   opts = function(_, opts)
     -- Modify LSP mappings
     opts.servers["*"].keys = vim.list_extend(opts.servers["*"].keys or {}, {
       { "<F2>", "<leader>cr", desc = "Rename", has = "rename", remap = true },
     })
-    -- Workspace diagnostics
-    Snacks.util.lsp.on(function(buffer, client)
-      if client.name ~= "copilot" then
-        require("workspace-diagnostics").populate_workspace_diagnostics(client, buffer)
-      end
-    end)
   end,
 }
