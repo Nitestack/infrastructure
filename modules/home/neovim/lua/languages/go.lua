65f1d792581cede932666955d616606b194a1006
Nitestack <code@npham.de>
2026-04-11 15:02:11 +0200

    feat(go): remove Go language support

--- modules/home/neovim/lua/languages/go.lua
+++
@@ -1,8 +0,0 @@
--- ╭─────────────────────────────────────────────────────────╮
--- │ Go                                                      │
--- ╰─────────────────────────────────────────────────────────╯
-return utils.plugin.get_language_spec({
-  plugins = {
-    { import = "lazyvim.plugins.extras.lang.go" },
-  },
-})
