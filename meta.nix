a877fe983f17ec569da0180f8fdefd26378b965e
Nitestack <code@npham.de>
2026-05-23 18:57:50 +0200

    feat(nix): update

--- meta.nix
+++ meta.nix
@@ -58,35 +58,4 @@
       variant = "mocha";
     };
   };
-  monitors = [
-    {
-      name = "DP-2";
-      resolution = "1920x1080";
-      refreshRate = 144;
-      position = {
-        x = 0;
-        y = 0;
-      };
-      backlight = {
-        i2cBus = "i2c-7";
-        device = "ddcci7";
-        busName = "AMDGPU DM aux hw bus 1"; # grep -r "AMDGPU DM aux hw bus" /sys/bus/i2c/devices/i2c-7/name
-      };
-    }
-    {
-      name = "DP-1";
-      resolution = "1920x1080";
-      refreshRate = 200;
-      position = {
-        x = 1920;
-        y = 0;
-      };
-      isDefault = true;
-      backlight = {
-        i2cBus = "i2c-6";
-        device = "ddcci6";
-        busName = "AMDGPU DM aux hw bus 0"; # 1rep -r "AMDGPU DM aux hw bus" /sys/bus/i2c/devices/i2c-6/name
-      };
-    }
-  ];
 }
