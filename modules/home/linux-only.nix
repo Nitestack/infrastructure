c0c91ca635cb66f4b278a9ff46eb32ea7b9de3dd
Nitestack <code@npham.de>
2026-06-18 11:01:39 +0200

    feat(wsl): use `ssh-agent` from Windows

--- modules/home/linux-only.nix
+++ modules/home/linux-only.nix
@@ -2,6 +2,5 @@
 # │ Linux Home Manager Configuration                         │
 # ╰──────────────────────────────────────────────────────────╯
 _: {
-  services.ssh-agent.enable = true;
   programs.zathura.enable = true;
 }
