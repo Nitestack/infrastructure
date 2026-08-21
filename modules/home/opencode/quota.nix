31dd35666c97430b128fd4de5143482730b81c02
Nitestack <code@npham.de>
2026-08-22 01:03:57 +0200

    feat(opencode): tune quota plugin display surfaces

--- modules/home/opencode/quota.nix
+++ modules/home/opencode/quota.nix
@@ -3,4 +3,8 @@
   formatStyle = "allWindows";
   enableToast = false;
   maintainerAnnouncements.enabled = false;
+  sessionTokenScope = "tree";
+  tuiCommandDisplay = "dialog";
+  tuiCompactStatus.enabled = true;
+  tuiSidebarPanel.enabled = false;
 }
